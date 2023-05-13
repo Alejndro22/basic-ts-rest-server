b2cad48d14472a73549aeabf50aeee084a3a3298
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-05-13 03:00:03 -0600

    endpoints for getUsers and getUserById working

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,17 +1,21 @@
 import { Request, Response } from 'express';
+import User from '../models/user';
 
-const getUsers = (req: Request, res: Response) => {
-  res.json({
-    msg: 'getUsers',
-  });
+const getUsers = async (req: Request, res: Response) => {
+  const users = await User.findAll();
+
+  res.json({ users });
 };
 
-const getUser = (req: Request, res: Response) => {
+const getUser = async (req: Request, res: Response) => {
   const { id } = req.params;
-  res.json({
-    msg: 'getUser',
-    id,
-  });
+  const user = await User.findByPk(id);
+
+  if (!user) {
+    res.status(404).json({ msg: `Couldn't find user with id:${id}` });
+  } else {
+    res.json({ user });
+  }
 };
 
 const postUser = (req: Request, res: Response) => {
